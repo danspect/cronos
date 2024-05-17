128bad269c0b19d277bdb1a295c5f7a5b44e037c
aztec <daniel_aspect@proton.me>
2024-05-17 18:48:20 -0300

    fixed cronometer logic

--- cronos/cronometer.py
+++ cronos/cronometer.py
@@ -1,22 +1,29 @@
 import time
 
-class cronometer():
+
+class Cronometer:
     def __init__(self) -> None:
         self.running = False
         self.start_time = 0
-        self.elapsed_time = time.time() - self.start_time
-    
+        self.elapsed_time = 0
+
     def start(self) -> None:
         if not self.running:
             self.running = True
             self.start_time = time.time()
-    
-    def pause(self) ->None:
-        pass
-    
+
+    def pause(self) -> None:
+        if self.running:
+            self.running = False
+            self.elapsed_time += time.time() - self.start_time
+
     def reset(self) -> None:
         self.running = False
         self.start_time = 0
+        self.elapsed_time = 0
 
     def get_time(self) -> int:
-        return self.elapsed_time
+        if self.running:
+            return self.elapsed_time + time.time() - self.start_time
+        else:
+            return self.elapsed_time
