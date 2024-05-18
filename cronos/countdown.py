aa1999dee93a2af8ac56743be4ab4ac88b2283c1
aztec <daniel_aspect@proton.me>
2024-05-18 08:40:40 -0300

    Created Countdown structure

--- cronos/countdown.py
+++ cronos/countdown.py
@@ -4,3 +4,19 @@ import time
 class Countdown:
     def __init__(self) -> None:
         pass
+
+    def start(self, time_sec) -> None:
+        pass
+    
+    def pause(self) -> None:
+        pass
+    
+    def reset(self) -> None:
+        pass
+
+    def get_remaining_time(self) -> str:
+        pass
+
+    def add_one_minute(self) -> None:
+        pass
+    
