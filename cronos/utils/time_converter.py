814e5acbb9bc2023d536b1233a9718619f37a364
aztec <daniel_aspect@proton.me>
2024-05-18 09:04:19 -0300

    Added a simple test

--- cronos/utils/time_converter.py
+++ cronos/utils/time_converter.py
@@ -21,3 +21,10 @@ def seconds_to_minutes(seconds):
 def seconds_to_hours(seconds):
     return seconds / 3600
 
+
+if __name__ == "__main__":
+    print("1 hora é igual a", hours_to_minutes(1), "minutos")
+    print("2 horas são iguais a", hours_to_seconds(2), "segundos")
+    print("300 segundos são iguais a", seconds_to_minutes(300), "minutos")
+    print("7200 segundos são iguais a", seconds_to_hours(7200), "horas")
+    print("120 minutos são iguais a", minutes_to_hours(120), "horas")
