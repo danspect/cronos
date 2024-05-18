0ec444d3b8be865a964350f43d0afe3901384759
aztec <daniel_aspect@proton.me>
2024-05-18 09:14:37 -0300

    Created app body

---
+++ cronos/app.py
@@ -0,0 +1,16 @@
+from cronos.utils import (
+    format_time,
+    Logger,
+    hours_to_minutes,
+    hours_to_seconds,
+    minutes_to_seconds,
+)
+import time
+from tkinter import *
+
+class App:
+    def __init__(self) -> None:
+        pass
+
+if __name__ == "__main__":
+    pass
