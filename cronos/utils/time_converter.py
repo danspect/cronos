c604ea013d93aa027879597e032c44aee451c98e
aztec <daniel_aspect@proton.me>
2024-05-18 08:46:27 -0300

    Created time converter logic

---
+++ cronos/utils/time_converter.py
@@ -0,0 +1,17 @@
+def hours_to_minutes(hours):
+    return hours * 60
+
+def hours_to_seconds(hours):
+    return hours * 3600
+
+def minutes_to_seconds(minutes):
+    return minutes * 60 
+
+def minutes_to_hours(minutes):
+    return minutes / 60
+
+def seconds_to_minutes(seconds):
+    return seconds / 60
+
+def seconds_to_hours(seconds):
+    return seconds / 3600
