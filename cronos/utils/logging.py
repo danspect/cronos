632445c202f1c3918e8fd60d6063b0ff9222d73c
aztec <daniel_aspect@proton.me>
2024-05-18 09:08:30 -0300

    added logger logic

--- cronos/utils/logging.py
+++ cronos/utils/logging.py
@@ -1,2 +1,23 @@
 import logging
 
+
+class Logger:
+    def __init__(self, name, log_file, level=logging.INFO) -> None:
+        self.logger = logging.getLogger(name)
+        self.logger.setLevel(level)
+
+        formatter = logging.Formatter(
+            "%(asctime)s - %(name)s - %(levelname)s - %(message)s"
+        )
+
+        file_handler = logging.FileHandler(log_file)
+        file_handler.setFormatter(formatter)
+
+        stream_handler = logging.StreamHandler()
+        stream_handler.setFormatter(formatter)
+
+        self.logger.addHandler(file_handler)
+        self.logger.addHandler(stream_handler)
+
+    def get_logger(self):
+        return self.logger
